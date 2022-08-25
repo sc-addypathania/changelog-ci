2fe51653d094257ac78800e64a679fcd8704f917
Addy Pathania <aditya.pathania22@gmail.com>
2022-08-25 18:37:03 -0400

    feature :added new feature 3

--- test1.js
+++ test1.js
@@ -1,3 +1,4 @@
 ﻿console.log("we are in github actions");
 console.log("adding another feature");
-console.log("adding another feature");
+console.log("adding another feature 2");
+console.log("adding another feature 3");
