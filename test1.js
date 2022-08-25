deb22286fbf0d131f8ec921e9e89f9d578ebf148
Addy Pathania <aditya.pathania22@gmail.com>
2022-08-25 19:11:16 -0400

    Release update 1.1.0.

--- test1.js
+++ test1.js
@@ -1,4 +1,5 @@
 ﻿﻿console.log("we are in github actions");
 console.log("adding another feature");
 console.log("adding another feature 2");
-console.log("adding another feature 3");
+console.log("adding another feature 2");
+console.log("addidng release");
